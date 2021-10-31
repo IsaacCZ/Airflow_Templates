e5dce010db26d3cf9236c06342f6e369150d33ad
IsaacCZ <34696445+IsaacCZ@users.noreply.github.com>
2021-10-31 13:21:14 -0600

    Add files via upload

---
+++ sql/Data_base.py
@@ -0,0 +1,28 @@
+import datetime
+
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+# create_pet_table, populate_pet_table, get_all_pets, and get_birth_date are examples of tasks created by
+# instantiating the Postgres Operator
+
+with DAG(
+    dag_id="postgres_operator_dag",
+    start_date=datetime.datetime(2020, 2, 2),
+    schedule_interval="@once",
+    catchup=False,
+) as dag:
+    create_pet_table = PostgresOperator(
+        task_id="create_user_purchase_table",
+        sql="""
+            CREATE TABLE IF NOT EXISTS user_purchase (
+            invoice_number varchar(20),
+            stock_code varchar(20),
+            detail varchar(1000),
+            quantity int,
+            inovoice_date timestamp,
+            unit_price numeric(8,3),
+            customer_id int,
+            country varchar(20);
+          """,
+    )
